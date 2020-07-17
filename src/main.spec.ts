65a5e7f697807d28484cc0903f77d81f0ee8b0b9
ccpu <m.doaie@hotmail.com>
2020-07-17 16:08:22 +0430

    added function support

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,18 +1,21 @@
 import { expect } from 'chai';
 import rtl from './main';
+import jssFunc from 'jss-plugin-rule-value-function';
+import { create, sheets } from 'jss';
+import jssNested from 'jss-plugin-nested';
+import jssGlobal from 'jss-plugin-global';
 
-const { create, sheets } = require('jss');
 const jssPresetDefault = require('jss-preset-default').default;
 
 describe('jss-rtl', () => {
   let jss: any;
 
   beforeEach(() => {
-    jss = create().use(rtl());
+    jss = create({ plugins: [jssFunc(), jssGlobal(), jssNested(), rtl()] });
   });
 
   afterEach(() => {
-    sheets.registry.forEach((sheet: any) => sheet.detach());
+    (sheets as any).registry.forEach((sheet: any) => sheet.detach());
     sheets.reset();
   });
 
@@ -28,13 +31,10 @@ describe('jss-rtl', () => {
     });
 
     it('should generate correct CSS', () => {
-      expect(sheet.toString()).to.be.equals([
-        '.a-0-2-1 {',
-        '  padding-right: 1px;',
-        '}',
-      ].join('\n'));
+      expect(sheet.toString()).to.be.equals(
+        ['.a-1-2-1 {', '  padding-right: 1px;', '}'].join('\n'),
+      );
     });
-
   });
 
   describe('global enable', () => {
@@ -50,22 +50,17 @@ describe('jss-rtl', () => {
     });
 
     it('should generate unchanged CSS', () => {
-      expect(sheet.toString()).to.be.equals([
-        '.a-0-6-1 {',
-        '  padding-left: 1px;',
-        '}',
-      ].join('\n'));
+      expect(sheet.toString()).to.be.equals(
+        ['.a-1-6-1 {', '  padding-left: 1px;', '}'].join('\n'),
+      );
     });
 
     it('should remove the flip property from the style even when disabled', () => {
       sheet = jss.createStyleSheet({ a: { flip: true, 'padding-left': '1px' } });
-      expect(sheet.toString()).to.be.equals([
-        '.a-0-8-2 {',
-        '  padding-left: 1px;',
-        '}',
-      ].join('\n'));
+      expect(sheet.toString()).to.be.equals(
+        ['.a-1-8-2 {', '  padding-left: 1px;', '}'].join('\n'),
+      );
     });
-
   });
 
   describe('create stylesheet opt-out', () => {
@@ -80,13 +75,10 @@ describe('jss-rtl', () => {
     });
 
     it('should generate unchanged CSS', () => {
-      expect(sheet.toString()).to.be.equals([
-        '.a-0-10-1 {',
-        '  padding-left: 1px;',
-        '}',
-      ].join('\n'));
+      expect(sheet.toString()).to.be.equals(
+        ['.a-1-10-1 {', '  padding-left: 1px;', '}'].join('\n'),
+      );
     });
-
   });
 
   describe('rule opt-out', () => {
@@ -105,16 +97,17 @@ describe('jss-rtl', () => {
     });
 
     it('should generate unchanged CSS and remove the flip prop', () => {
-      expect(sheet.toString()).to.be.equals([
-        '.a-0-12-1 {',
-        '  padding-right: 1px;',
-        '}',
-        '.b-0-12-2 {',
-        '  padding-left: 1px;',
-        '}',
-      ].join('\n'));
+      expect(sheet.toString()).to.be.equals(
+        [
+          '.a-1-12-1 {',
+          '  padding-right: 1px;',
+          '}',
+          '.b-1-12-2 {',
+          '  padding-left: 1px;',
+          '}',
+        ].join('\n'),
+      );
     });
-
   });
 
   describe('rule opt-in', () => {
@@ -134,14 +127,16 @@ describe('jss-rtl', () => {
     });
 
     it('should generate changed CSS and remove the flip prop', () => {
-      expect(sheet.toString()).to.be.equals([
-        '.a-0-16-1 {',
-        '  padding-left: 1px;',
-        '}',
-        '.b-0-16-2 {',
-        '  padding-right: 1px;',
-        '}',
-      ].join('\n'));
+      expect(sheet.toString()).to.be.equals(
+        [
+          '.a-1-16-1 {',
+          '  padding-left: 1px;',
+          '}',
+          '.b-1-16-2 {',
+          '  padding-right: 1px;',
+          '}',
+        ].join('\n'),
+      );
     });
   });
 
@@ -173,20 +168,22 @@ describe('jss-rtl', () => {
     });
 
     it('should generate multiple font-face rules', () => {
-      expect(sheet.toString()).to.be.equals([
-        '@font-face {',
-        '  font-family: Roboto;',
-        '  font-style: normal;',
-        '  font-wieght: normal;',
-        '  src: url(/fonts/Roboto.woff2) format("woff2");',
-        '}',
-        '@font-face {',
-        '  font-family: Roboto;',
-        '  font-style: normal;',
-        '  font-wieght: 300;',
-        '  src: url(/fonts/Roboto-Light.woff2) format("woff2");',
-        '}',
-      ].join('\n'));
+      expect(sheet.toString()).to.be.equals(
+        [
+          '@font-face {',
+          '  font-family: Roboto;',
+          '  font-style: normal;',
+          '  font-wieght: normal;',
+          '  src: url(/fonts/Roboto.woff2) format("woff2");',
+          '}',
+          '@font-face {',
+          '  font-family: Roboto;',
+          '  font-style: normal;',
+          '  font-wieght: 300;',
+          '  src: url(/fonts/Roboto-Light.woff2) format("woff2");',
+          '}',
+        ].join('\n'),
+      );
     });
   });
 
@@ -234,32 +231,92 @@ describe('jss-rtl', () => {
     });
 
     it('should generate space or comma separated values', () => {
-      expect(sheet.toString()).to.be.equals([
-        '@font-face {',
-        '  src: url(/font/Roboot.woff2) format("woff2");',
-        '}',
-        '@media(min-width: 480px) {',
-        '  body {',
-        '    padding: 10px 40px 30px 20px;',
-        '  }',
-        '}',
-        'body {',
-        '  padding: 1px 4px 3px 2px;',
-        '}',
-        'body-no-flip {',
-        '  padding: 1px 2px 3px 4px;',
-        '}',
-        '.button-0-22-1 {',
-        '  border: 1px solid red, 2px solid blue;',
-        '  margin: 1px 4px 3px 2px !important;',
-        '  padding: 1px 4px 3px 2px;',
-        '}',
-        '.button-no-flip-0-22-2 {',
-        '  border: 1px solid red, 2px solid blue;',
-        '  margin: 1px 2px 3px 4px !important;',
-        '  padding: 1px 2px 3px 4px;',
-        '}',
-      ].join('\n'));
+      expect(sheet.toString()).to.be.equals(
+        [
+          '@font-face {',
+          '  src: url(/font/Roboot.woff2) format("woff2");',
+          '}',
+          '@media(min-width: 480px) {',
+          '  body {',
+          '    padding: 10px 40px 30px 20px;',
+          '  }',
+          '}',
+          'body {',
+          '  padding: 1px 4px 3px 2px;',
+          '}',
+          'body-no-flip {',
+          '  padding: 1px 2px 3px 4px;',
+          '}',
+          '.button-1-22-1 {',
+          '  border: 1px solid red, 2px solid blue;',
+          '  margin: 1px 4px 3px 2px !important;',
+          '  padding: 1px 4px 3px 2px;',
+          '}',
+          '.button-no-flip-1-22-2 {',
+          '  border: 1px solid red, 2px solid blue;',
+          '  margin: 1px 2px 3px 4px !important;',
+          '  padding: 1px 2px 3px 4px;',
+          '}',
+        ].join('\n'),
+      );
     });
   });
+
+  // describe('dynamic value', () => {
+  //   it('should flip rule with function', () => {
+  //     const sheet = jss.createStyleSheet({
+  //       a: { marginLeft: (p: any) => p.marginLeft },
+  //     });
+  //     sheet.update({ marginLeft: 10 });
+  //     const style = sheet.toString().split('\n').join('');
+  //     expect(style).to.be.equals('.a-1-1-1 {  marginRight: 10;}');
+  //   });
+
+  //   it('should not flip rule with function', () => {
+  //     const sheet = jss.createStyleSheet({
+  //       a: { marginLeft: (p: any) => p.marginLeft, flip: false },
+  //     });
+  //     sheet.update({ marginLeft: 10 });
+  //     const style = sheet.toString().split('\n').join('');
+  //     expect(style).to.be.equals('.a-1-2-1 {  marginLeft: 10;}');
+  //   });
+
+  //   it('should flip function rules', () => {
+  //     const sheet = jss.createStyleSheet({
+  //       a: (p) => ({
+  //         marginLeft: p.marginLeft,
+  //         left: 10,
+  //       }),
+  //     });
+  //     sheet.update({ marginLeft: 10 });
+  //     const style = sheet.toString().split('\n').join('');
+  //     expect(style).to.be.equals('.a-1-3-1 {  marginRight: 10;  right: 10;}');
+  //   });
+
+  //   it('should not flip function rules', () => {
+  //     const sheet = jss.createStyleSheet({
+  //       a: (p) => ({
+  //         flip: false,
+  //         marginLeft: p.marginLeft,
+  //         left: 10,
+  //       }),
+  //     });
+  //     sheet.update({ marginLeft: 10 });
+  //     const style = sheet.toString().split('\n').join('');
+  //     expect(style).to.be.equals('.a-1-4-1 {  marginLeft: 10;  left: 10;}');
+  //   });
+
+  //   it('should work on nested', () => {
+  //     const sheet = jss.createStyleSheet({
+  //       a: {
+  //         '&:hover': {
+  //           marginLeft: (p: any) => p.marginLeft,
+  //         },
+  //       },
+  //     });
+  //     sheet.update({ marginLeft: 20 });
+  //     const style = sheet.toString().split('\n').join('');
+  //     expect(style).to.be.equals('.a-1-5-1:hover {  marginRight: 20;}');
+  //   });
+  // });
 });
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,36 @@ export interface JssRTLOptions {
   opt?: 'in' | 'out';
 }
 
-export default function jssRTL({ enabled = true, opt = 'out' }: JssRTLOptions = {}) {
+const getRuleFunctions = (rule: any) => {
+  return Object.keys(rule).reduce((funcs, key) => {
+    if (key.startsWith('fnValues')) {
+      var fnValues = rule[key];
+      if (Object.keys(fnValues).length !== 0) {
+        funcs.push(fnValues);
+      }
+    }
+    return funcs;
+  }, [] as any[]);
+};
+
+const shouldFlip = (sheet: any, style: any, opt = 'out', removeFlipRule = false) => {
+  let flip = opt === 'out'; // If it's set to opt-out, then it should flip by default
+
+  if (typeof sheet.options.flip === 'boolean') {
+    flip = sheet.options.flip;
+  }
+
+  if (typeof style.flip === 'boolean') {
+    flip = style.flip;
+
+    if (removeFlipRule) {
+      delete style.flip;
+    }
+  }
+  return flip;
+};
+
+export default function jssRTL({ enabled = true, opt = 'out' }: JssRTLOptions = {}): any {
   return {
     onProcessStyle(style: any, rule: any, sheet: any) {
       if (rule.type === 'font-face') {
@@ -22,16 +51,7 @@ export default function jssRTL({ enabled = true, opt = 'out' }: JssRTLOptions = 
         return style;
       }
 
-      let flip = opt === 'out'; // If it's set to opt-out, then it should flip by default
-
-      if (typeof sheet.options.flip === 'boolean') {
-        flip = sheet.options.flip;
-      }
-
-      if (typeof style.flip === 'boolean') {
-        flip = style.flip;
-        delete style.flip;
-      }
+      let flip = shouldFlip(sheet, style, opt, getRuleFunctions(rule).length === 0);
 
       if (!flip) {
         return style;
@@ -39,5 +59,43 @@ export default function jssRTL({ enabled = true, opt = 'out' }: JssRTLOptions = 
 
       return convert(typeof rule.toJSON === 'function' ? rule.toJSON() : style);
     },
+    // onUpdate(data: object, rule: any, sheet?: any, options?: any) {
+    //   getRuleFunctions(rule).forEach((fnValues) => {
+    //     let flip = shouldFlip(sheet, rule.style, opt, true);
+
+    //     if (enabled && flip && fnValues) {
+    //       for (var _prop in fnValues) {
+    //         const value = fnValues[_prop](data);
+
+    //         const rtlStyle = convert({ [_prop]: value });
+
+    //         const convertedRule = Object.keys(rtlStyle)[0];
+
+    //         if (convertedRule !== _prop) {
+    //           rule.prop(_prop, null, options);
+    //         }
+
+    //         rule.prop(convertedRule, value, options);
+    //       }
+    //     }
+    //   });
+    // },
   };
+  // onChangeValue(value: any, prop: any, rule: any) {
+  //   if (prop === 'marginRight' || prop === 'margin-right') {
+  //     return value;
+  //   }
+
+  //   if (rule.__flip) {
+  //     const styles = convert({ [prop]: value });
+  //     const key = Object.keys(styles)[0];
+  //     if (key !== prop) {
+  //       rule.prop(key, value, { process: false });
+  //     }
+  //     delete rule.__flip;
+  //     return false;
+  //   }
+
+  //   return value;
+  // },
 }
